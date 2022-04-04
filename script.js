03f58f8ab8ab2a253e1c9a1874a9e304ee7e9008
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-04-04 17:26:02 -0300

    nave descendo e subindo

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 const yourShip = document.querySelector('.player-shooter');
 const playArea = document.querySelector('#main-play-game');
-
+//Movimento e tiro da nave
 function flyShip(event){
     if(event.key === 'ArrowUp'){
         event.preventDefault();
         moveUp();
     } else if(event.key === 'ArrowDown'){
         event.preventDefault();
-        moveUp();
+        moveDown();
     } else if(event.key === " "){
         event.preventDefault();
         fireLaser();
     }
 }
+
+//Função de subir
+ function moveUp(){
+     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+     if (topPosition === '0px') {
+         return
+     } else {
+        let position = parseInt(topPosition);
+        position -= 50;
+        yourShip.style.top = `${position}px`;
+     }
+ }
+
+//Função de descer
+ function moveDown(){
+     let topPosition = getComputedStyle(yourShip).getPropertyValue('top');
+     if (topPosition === '550px') {
+         return
+     } else {
+        let position = parseInt(topPosition);
+        position += 50;
+        yourShip.style.top = `${position}px`;
+     }
+ }
+
+
+ 
+ window.addEventListener('keydown', flyShip);
